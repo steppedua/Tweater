519aac5f9764518c8e5d1fe7179e1259b080ccac
Stepanyan Eduard <stepanyan.edua@gmail.com>
2020-10-05 23:39:03 +0300

    Add freemarker

--- src/main/java/com/steppedua/sweater/controller/MainController.java
+++ src/main/java/com/steppedua/sweater/controller/MainController.java
@@ -6,6 +6,7 @@ import com.steppedua.sweater.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,12 +16,8 @@ import java.util.Map;
 @Controller
 public class MainController {
 
-    private final MessageRepository messageRepository;
-
     @Autowired
-    public MainController(MessageRepository messageRepository) {
-        this.messageRepository = messageRepository;
-    }
+    private MessageRepository messageRepository;
 
     @GetMapping("/")
     public String greeting(Map<String, Object> model) {
@@ -28,10 +25,17 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Message> messages = messageRepository.findAll();
 
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
 
         return "main";
     }
@@ -40,28 +44,13 @@ public class MainController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag,
-            Map<String, Object> model) {
+            @RequestParam String tag, Map<String, Object> model
+    ) {
         Message message = new Message(text, tag, user);
 
         messageRepository.save(message);
 
         Iterable<Message> messages = messageRepository.findAll();
-
-        model.put("messages", messages);
-
-        return "main";
-    }
-
-    @PostMapping("/filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepository.findByTag(filter);
-        } else {
-            messages = messageRepository.findAll();
-        }
 
         model.put("messages", messages);
 
--- src/main/java/com/steppedua/sweater/controller/RegistrationController.java
+++ src/main/java/com/steppedua/sweater/controller/RegistrationController.java
@@ -5,7 +5,6 @@ import com.steppedua.sweater.model.User;
 import com.steppedua.sweater.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -14,12 +13,8 @@ import java.util.Map;
 
 @Controller
 public class RegistrationController {
-    private final UserRepository userRepository;
-
     @Autowired
-    public RegistrationController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private UserRepository userRepository;
 
     @GetMapping("/registration")
     public String registration() {
@@ -30,7 +25,7 @@ public class RegistrationController {
     public String addUser(User user, Map<String, Object> model) {
         User userFromDb = userRepository.findByUsername(user.getUsername());
 
-        if (userFromDb != null){
+        if (userFromDb != null) {
             model.put("message", "User exists!");
             return "registration";
         }
@@ -38,6 +33,7 @@ public class RegistrationController {
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
         userRepository.save(user);
+
         return "redirect:/login";
     }
 }

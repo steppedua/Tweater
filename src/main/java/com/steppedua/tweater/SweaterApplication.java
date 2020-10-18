a2374bb46d4a07424742f8d0a60082fb348794d1
Stepanyan Eduard <stepanyan.edua@gmail.com>
2020-10-19 01:24:22 +0300

    Add JDK 11

--- src/main/java/com/steppedua/tweater/SweaterApplication.java
+++ src/main/java/com/steppedua/tweater/SweaterApplication.java
@@ -1,9 +1,17 @@
 package com.steppedua.tweater;
 
+import com.steppedua.tweater.repository.MessageRepository;
+import com.steppedua.tweater.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@Configuration
+@EnableJpaRepositories
+@Import({UserRepository.class, MessageRepository.class})
 public class SweaterApplication {
     public static void main(String[] args) {
         SpringApplication.run(SweaterApplication.class, args);
